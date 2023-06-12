209cd69c3c46523ca8c05997a7fa5939fa86254a
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-06-12 23:53:26 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 46584 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,12 @@
+N = int(input())
+dist = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+result = 0
+c = cost[0]
+
+for i in range(N-1):
+    if c > cost[i]:
+        c = cost[i]
+    result += c*dist[i]
+
+print(result)
