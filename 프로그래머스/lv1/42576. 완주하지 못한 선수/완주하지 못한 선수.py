bf4d4f6f8808e4d72c3b71a723848cd0de7a3b1d
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-07-04 23:49:09 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -7,3 +7,22 @@ def solution(participant, completion):
     
     # 4. counter의 key값을 반환한다
     return list(answer.keys())[0]
+
+# 더 이해되고 좋다고 생각되는 답
+def solution(participant, completion):
+    hashDict = {}
+    sumHash = 0
+    
+    # 1. Hash : Participant의 dictionary 만들기
+    # 2. Participant의 sum(hash) 구하기
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
+    
+    # 3. completion의 sum(hash) 빼기
+    for comp in completion:
+        sumHash -= hash(comp)
+    
+    # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다
+
+    return hashDict[sumHash]
