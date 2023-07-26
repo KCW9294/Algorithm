54bb456e6ba953af2d85668d6af1021cfddc0ca4
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-07-26 19:17:54 +0900

    Create 이중우선순위큐 다른버전.py

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐 다른버전.py
@@ -0,0 +1,29 @@
+import heapq
+def solution(operations):
+    max_heap = []
+    min_heap = []
+    answer = []
+    temp = []
+    
+    for op in operations:
+        num = op.split(' ')
+        if num[0] == "I":
+            value = int(num[1])
+            heapq.heappush(max_heap,(-value,value))
+            heapq.heappush(min_heap,(value))
+        else:
+            if len(max_heap) == 0:
+                pass
+            elif num[1] == "1" and max_heap:
+                max_num = heapq.heappop(max_heap)[1]
+                min_heap.remove(max_num)
+            elif num[1] == "-1" and min_heap:
+                min_num = heapq.heappop(min_heap)
+                max_heap.remove((-min_num,min_num))
+                
+    if len(max_heap) != 0:
+        answer = [max_heap[0][1],min_heap[0]]
+    else:
+        answer = [0,0]
+    
+    return answer
