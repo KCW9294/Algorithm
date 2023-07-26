12b644ddffef6bc9b81a5b5cbeee490b66a05325
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-07-26 19:15:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,19 +1,31 @@
+import heapq
 def solution(operations):
-    lst = []
-    heap = []
+    max_heap = []
+    min_heap = []
     answer = []
-    for operation in operations:
-        lst.append(operation.split(' '))
-    for op in lst:
-        if op[0]=='I':
-            heap.append(int(op[1]))
-        if op[0]=='D' and op[1]=="1" and heap:
-            heap.remove(max(heap))
-        if op[0]=='D' and op[1]=="-1" and heap:
-            heap.remove(min(heap))
-    if heap:
-        answer = [max(heap),min(heap)]
+    temp = []
+    
+    for op in operations:
+        num = op.split(' ')
+        if num[0] == "I":
+            heapq.heappush(temp,int(num[1]))
+            heapq.heappush(max_heap,(-int(num[1]),int(num[1])))
+            heapq.heappush(min_heap,int(num[1]))
+        else:
+            if len(temp) == 0:
+                pass
+            elif num[1] == "1" and max_heap:
+                max_num = heapq.heappop(max_heap)[1]
+                min_heap.remove(max_num)
+                temp.remove(max_num)
+            elif num[1] == "-1" and min_heap:
+                min_num = heapq.heappop(min_heap)
+                max_heap.remove((-min_num,min_num))
+                temp.remove(min_num)
+                
+    if len(temp) != 0:
+        answer = [max_heap[0][1],min_heap[0]]
     else:
         answer = [0,0]
-            
+    
     return answer
