b6e7827f6ff260653b62d74122a1a940ddba2607
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-08-02 18:00:30 +0900

    [level 2] Title: n^2 배열 자르기, Time: 26.31 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,13 +1,9 @@
 def solution(n, left, right):
     answer = []
     for i in range(left, right+1):
-        if i%n == n-1:
-            answer.append(n)
-            continue
+        if i//n<=i%n:
+            answer.append(i%n+1)
         else:
-            if i//n<=i%n:
-                answer.append(i%n+1)
-            else:
-                answer.append(i//n+1)
+            answer.append(i//n+1)
         
     return answer
