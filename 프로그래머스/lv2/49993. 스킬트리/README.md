a8bb3b4fbcef27c3c47888032e83387a117b1933
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-08-05 20:36:55 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/README.md
+++ 프로그래머스/lv2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,21 +1,18 @@
 def solution(skill, skill_trees):
     answer = 0
+
     for tree in skill_trees:
-        temp = -1
-        break_go = False
+        idx = 0
+        possible = True
+
+        for s in tree:
+            if s in skill:
+                if s != skill[idx]:
+                    possible = False
+                    break
+                idx += 1
         
-        for i in range(len(tree)): 
-            for j in range(len(skill)):
-                if tree[i] == skill[j]:
-                    temp += 1
-                    if temp != j:
-                        break_go = True
-                        break
-            if break_go == True:
-                break
-        if break_go == False:
+        if possible:
             answer += 1
 
-                            
-                
     return answer
