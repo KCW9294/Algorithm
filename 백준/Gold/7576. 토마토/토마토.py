ab51fc02476d30df0fea7be57566d90490388772
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-08-18 18:00:38 +0900

    [Gold V] Title: 토마토, Time: 2748 ms, Memory: 98600 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,34 @@
+from collections import deque
+M,N = map(int,input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+max_num = 0
+answer = 0
+x=0
+y=0
+deq = deque()
+nx = [1,-1,0,0]
+ny = [0,0,1,-1]
+for i in range(N):
+    for j in range(M):
+        if graph[i][j]==1:
+            deq.append((i,j))
+
+while deq:
+    x,y = deq.popleft()
+    for i in range(4):
+        if 0<=x+nx[i]<N and 0<=y+ny[i]<M:
+            if graph[x+nx[i]][y+ny[i]]==0:
+                graph[x+nx[i]][y+ny[i]]=graph[x][y]+1
+                deq.append((x+nx[i],y+ny[i]))
+
+for i in graph:
+    if 0 in i:
+        answer = -1
+        break
+    else:
+        if max(i)>max_num:
+            max_num = max(i)
+if answer == 0:
+    print(max_num-1)
+else:
+    print(answer)
