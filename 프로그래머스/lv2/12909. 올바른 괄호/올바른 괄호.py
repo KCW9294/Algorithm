9d60e4f501fdc88fe34c9f32b727da75e3a71eb7
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-09-01 17:33:36 +0900

    [level 2] Title: 올바른 괄호, Time: 6.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,20 +1,49 @@
-def solution(s):
-    answer = True
-    temp = []
-    for i in range(len(s)):
-        if s[0] == ')':
-            answer = False
-            break
-        if s[i] == '(':
-            temp.append(s[i])
-        else:
-            if len(temp)==0:
-                answer = False
-                break
-            temp.pop()
-    if len(temp) == 0 and answer == True:
-        answer = True
-    else:
-        answer = False
+# def solution(s):
+#     answer = True
+#     temp = []
+#     for i in range(len(s)):
+#         if s[0] == ')':
+#             answer = False
+#             break
+#         if s[i] == '(':
+#             temp.append(s[i])
+#         else:
+#             if len(temp)==0:
+#                 answer = False
+#                 break
+#             temp.pop()
+#     if len(temp) == 0 and answer == True:
+#         answer = True
+#     else:
+#         answer = False
 
-    return answer
+#     return answer
+
+
+def solution(s):
+    answer = 0
+    for i in s:
+        if i=='(':
+            answer += 1
+        else:
+            answer -= 1
+        if answer<0:
+            return False
+    if answer == 0:
+        return True
+    else:
+        return False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
