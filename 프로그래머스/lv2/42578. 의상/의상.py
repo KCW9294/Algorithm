ffed0f162f8bec93b0447d89a24fbf67e55bc8b7
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-09-01 16:55:00 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,14 +1,29 @@
+# def solution(clothes):
+#     hashlist = {}
+#     answer = 1
+#     for cloth in clothes:
+#         hashlist[hash(cloth[1])] = 0
+#     hashkey = hashlist.keys()
+#     print(hashkey)
+#     for cloth in clothes:
+#         for j in hashkey:
+#             if hash(cloth[1])==j:
+#                 hashlist[hash(cloth[1])] += 1
+#     result = list(hashlist.values())
+#     for i in result:
+#         answer *= i+1
+#     return answer-1
+
 def solution(clothes):
-    hashlist = {}
+    dic = {}
     answer = 1
     for cloth in clothes:
-        hashlist[hash(cloth[1])] = 0
-    hashkey = hashlist.keys()
-    for cloth in clothes:
-        for j in hashkey:
-            if hash(cloth[1])==j:
-                hashlist[hash(cloth[1])] += 1
-    result = list(hashlist.values())
-    for i in result:
-        answer *= i+1
+        if cloth[1] not in dic:
+            dic[cloth[1]] = 1
+        else:
+            dic[cloth[1]] += 1
+            
+    for i in dic:
+        answer *= dic[i]+1
+        
     return answer-1
