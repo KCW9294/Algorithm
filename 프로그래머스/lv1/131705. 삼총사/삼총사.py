1e4616f41fc50b1b749d325e8b4e5621203cd5e8
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-09-11 22:58:08 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+def solution(number):
+    answer = 0
+    for three in combinations(number,3):
+        if sum(three)==0:
+            answer += 1
+    return answer
+
