fe30636655b4b0d57b79525bea3f50b60697d983
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-09-11 23:24:26 +0900

    [unrated] Title: 기사단원의 무기, Time: 4.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -2,16 +2,12 @@ def solution(number, limit, power):
     lst = []
     for i in range(1,number+1):
         temp = 0
-        if i**0.5==round(i**0.5,0):
-            for k in range(1,int(i**0.5)):
-                if i%k==0:
-                    temp += 1
-            lst.append(temp*2+1)
-        else:
-            for j in range(1,int(i**0.5+1)):
-                if i%j==0:
-                    temp += 1
-            lst.append(temp*2)
+        for j in range(1,int(i**0.5+1)):
+            if i**0.5==j:
+                temp += 1
+            elif i%j==0:
+                temp += 2
+        lst.append(temp)
     lst.sort()
     for i in range(len(lst)):
         if lst[i]>limit:
