87684c6ebb70f59012ac9a49e34979298cb72846
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-10-01 20:33:25 +0900

    [unrated] Title: 덧칠하기, Time: 1.27 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,10 +1,14 @@
 def solution(n, m, section):
     answer = 0
     temp = 0
+    # 오름차순이므로 값을 순서대로 꺼내줌
     for i in section:
+        # temp보다 작거나 같으면 이미 칠해진 구역이라 판단하고 continue
         if i<=temp:
             continue
+        # i값이 temp보다 크면 temp를 다시 초기화해주고 페인트칠 횟수를 증가시켜줌
         else:
+            # temp를 section에서 꺼낸 구역 번호 + 롤러 길이 값이라 지정
             temp = i+m-1
             answer += 1
     return answer
