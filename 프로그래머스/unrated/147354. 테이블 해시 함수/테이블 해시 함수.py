2a5c7b01e248d4533e647d413ba34952b90d7a87
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-10-02 13:39:38 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,8 +1,8 @@
 def solution(data, col, row_begin, row_end):
     answer = 0
     temp = []
-    data.sort(key=lambda x:x[0], reverse=True)
-    data.sort(key=lambda x:x[col-1])
+    # col번째 컬럼의 값 기준으로 오름차순 정렬후 첫 번째 컬럼 값 기준으로 내림차순 정렬
+    data.sort(key=lambda x:[x[col-1],-x[0]])
     for i in range(row_begin, row_end+1):
         temp_temp = 0
         for k in data[i-1]:
