ca3ecfcb708e970b4327b6d4324344da53e7b266
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-10-05 01:18:11 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,17 +1,15 @@
 def solution(N, stages):
-    failure_rates = []
-    total_players = len(stages)
-    
-    for stage in range(1, N+1):
-        if total_players == 0:
-            failure_rate = 0
-        else:
-            players_on_stage = stages.count(stage)
-            failure_rate = players_on_stage / total_players
-        failure_rates.append((stage, failure_rate))
-        total_players -= players_on_stage
-    
-    failure_rates.sort(key=lambda x: (-x[1], x[0]))  # 실패율 내림차순, 스테이지 번호 오름차순으로 정렬
-    
-    answer = [stage for stage, _ in failure_rates]
-    return answer
+    answer = []
+    people = len(stages)
+    result = []
+    for i in range(1,N+1):
+        if people == 0:
+            answer.append((i,0))
+            continue
+        temp = stages.count(i)
+        answer.append((i,temp/people))
+        people -= temp
+    answer.sort(key=lambda x:(-x[1],x[0]))
+    for i,j in answer:
+        result.append(i)
+    return result
