6f4df3253732d3610f5bd859aab208c689f5b0cc
김철우 <100820366+KCW9294@users.noreply.github.com>
2023-10-16 18:17:19 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,46 +1,43 @@
 from collections import deque
 
 def bfs(start, end, maps):
-	# 상하좌우
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1] 
-    visited = [[False]*len(maps[0]) for _ in range(len(maps))]
-    que = deque()
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    n = len(maps)
+    m = len(maps[0])
+    q = deque()
     out = False
+    visited = [[False]*m for _ in range(n)]
     
-    # 초깃값 설정
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-        	# 출발하고자 하는 지점이라면 시작점의 좌표를 기록함
-            if maps[i][j] == start:      
-                que.append((i, j, 0))    
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j]==start:
+                q.append((i,j,0))
                 visited[i][j] = True
-                flag = True; break 
-        if out: break
-                
-    while que:
-        x, y, cost = que.popleft()
-        
-        if maps[x][y] == end:
-            return cost
-        
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            
-            if 0<= nx <len(maps) and 0<= ny <len(maps[0]) and maps[nx][ny] !='X' and visited[nx][ny]==False:
-                que.append((nx, ny, cost+1))
-                visited[nx][ny] = True
-                    
-    return -1	# 탈출할 수 없다면
-        
-def solution(maps):
-    path1 = bfs('S', 'L', maps)
-    path2 = bfs('L', 'E', maps)
+                out = True
+                break
+        if out:
+            break
     
-    # 둘다 -1 이 아니라면 탈출할 수 있음
-    if path1 != -1 and path2 != -1:
-        return path1 + path2
-        
-   	# 둘중 하나라도 -1 이면 탈출할 수 없음
+    while q:
+        x,y,cost = q.popleft()
+        if maps[x][y]==end:
+            return cost
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<m and visited[nx][ny]==False and maps[nx][ny]!='X':
+                visited[nx][ny]=True
+                q.append((nx,ny,cost+1))
+    
     return -1
+                
+def solution(maps):
+    result1 = bfs('S','L',maps)
+    result2 = bfs('L','E',maps)
+    
+    if result1!=-1 and result2!=-1:
+        return result1+result2
+    else:
+        return -1
